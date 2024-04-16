c9490e13a4b9f5161b016232e47a3f877ce808ba
Pranav Ramesh <laphatize@protonmail.com>
2024-04-16 16:23:34 -0400

    feat: auth trigger does not require auth

--- src/middleware/requireApiToken.js
+++ src/middleware/requireApiToken.js
@@ -12,6 +12,14 @@ const apiToken = process.env.API_TOKEN; // Assuming your .env file has the API t
  */
 
 const requireApiToken = (req, res, next) => {
+
+  // check if request is to the /api/containers/:id/login route
+  if (req.url.includes('/api/containers/') && req.url.endsWith('/login')) {
+    next();
+    return;
+  }
+
+  
   const token = req.headers['authorization'];
 
   if (!token) {
